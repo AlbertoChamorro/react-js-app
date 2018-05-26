1e82f10164514ede135ed3b61faccd17f8e06897
Alberto Chamorro <albertchc04@gmail.com>
2018-05-26 16:08:00 -0600

    create component with reactjs

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Media from './src/play-list/components/media';
 //console.log('hello world');
 
-const app = document.getElementById('app');
-const gretting = <h1>hello world!</h1>;
+// const app = document.getElementById('app');
+// const gretting = <h1>hello world!</h1>;
 
-ReactDOM.render(gretting, app);
+//ReactDOM.render(gretting, app);
+
+
+ReactDOM.render(<Media/>, app);
---
+++ src/play-list/components/media.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Media extends React.Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <h1>Purpose</h1>
+                    <img rsc="" alt="" width={260} height={160}/>
+                    <h3>Something React.js</h3>
+                    <p>by Alberto Chamorro</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Media;
