dde0659661454b94029a8447602bc642ac9911c7
Alberto Chamorro <albertchc04@gmail.com>
2018-05-26 15:19:56 -0600

    hello wrold with react

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+//console.log('hello world');
+
+const app = document.getElementById('app');
+const gretting = <h1>hello world!</h1>;
+
+ReactDOM.render(gretting, app);
