bbd7a7096f76080391a96fd9483b6e602c792e48
Alberto Chamorro <albertchc04@gmail.com>
2018-05-26 17:15:02 -0600

    properties in react js

--- src/play-list/components/media.js
+++ src/play-list/components/media.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
+import './media.css'
 
 class Media extends Component {
     render(){
+        const styles = {
+            container: { 
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '0.5px solid red',
+            }
+        }
         return (
-            <div>
+            //  style={styles.container}
+            <div className="Media">
                 <div>
-                    <h1>Purpose</h1>
-                    <img rsc="" alt="" width={260} height={160}/>
+                    <img src="./images/bitcoin.jpeg" alt="" width={260} height={160}/>
                     <h3>Something React.js</h3>
                     <p>by Alberto Chamorro</p>
                 </div>
             </div>
-        );
+        )
     }
 }
 
